27ab15511ad4eb1eb638a38fe9e1890f7895de2a
Ulf Ejlertsson <ulf.ejlertsson@gmail.com>
2018-05-15 09:25:25 +0200

    First commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+
+extern crate dotenv;
+
+#[macro_use]
+extern crate dotenv_codegen;
+
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
+
+    info!("starting up");
+
+    dotenv().ok();
+    for (key, value) in env::vars() {
+        println!(" -- {} : {}", key, value);
+    }
+
+    println!("{}", dotenv!("MEANING_OF_LIFE"));
+
     println!("Hello, world!");
 }
